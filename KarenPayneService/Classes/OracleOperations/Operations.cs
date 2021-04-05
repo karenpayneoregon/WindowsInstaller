383c56ac6f81580bd0a98507bd2a29fe10baf242
Karen Payne <Karen.1.Payne@oregon.gov>
2021-04-05 14:53:48 -0700

    test this out

--- KarenPayneService/Classes/OracleOperations/Operations.cs
+++ KarenPayneService/Classes/OracleOperations/Operations.cs
@@ -87,7 +87,6 @@ namespace KarenPayneService.Classes.OracleOperations
                             return null;
                         }
                         
-
                     }
                     catch (Exception exception)
                     {
